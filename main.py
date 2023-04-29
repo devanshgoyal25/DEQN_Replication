3d31903fa78ffe0b7191158622e7e1bd5eda3fda
devanshgoyal25 <78811149+devanshgoyal25@users.noreply.github.com>
2023-04-29 19:39:30 -0400

    Defined constants

--- main.py
+++ main.py
@@ -11,6 +11,67 @@ import matplotlib as plt
 
 print('Tensorflow Version: ', tf.__version__)
 
+seed = 1
+num_hidden_nodes = [100, 50]
+activations_hidden_nodes = [tf.nn.relu, tf.nn.relu]
+optimizer = 'adam'
+num_episodes = 50000
+len_episodes = 10240
+epochs_per_episode = 20
+minibatch_size = 512
+num_minibatch = int(len_episodes/minibatch_size)
+lr = 1e-5
+
+
+num_agents = A = 6
+num_exogenous_shocks = 4
+
+# Number of agents and exogenous shocks
+num_agents = A = 6
+num_exogenous_shocks = 4
+
+# Exogenous shock values
+delta = tf.constant([[0.5], [0.5], [0.9], [0.9]],  dtype=tf.float32)  # Capital depreciation (dependent on shock)
+eta = tf.constant([[0.95], [1.05], [0.95], [1.05]], dtype=tf.float32)  # TFP shock (dependent on shock)
+
+# Transition matrix
+# In this example we hardcoded the transition matrix. Changes cannot be made without also changing
+# the corresponding code below.
+p_transition = 0.25  # All transition probabilities are 0.25
+pi_np = p_transition * np.ones((4, 4))  # Transition probabilities
+pi = tf.constant(pi_np, dtype=tf.float32)  # Transition probabilities
+
+# Labor endowment
+labor_endow_np = np.zeros((1, A))
+labor_endow_np[:, 0] = 1.0  # Agents only work in their first period
+labor_endow = tf.constant(labor_endow_np, dtype=tf.float32)
+
+# Production and household parameters
+alpha = tf.constant(0.3)  # Capital share in production
+beta_np = 0.7
+beta = tf.constant(beta_np, dtype=tf.float32)  # Discount factor (patience)
+gamma = tf.constant(1.0, dtype=tf.float32)  # CRRA coefficient
+
+if optimizer == 'adam':
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+else:
+    raise NotImplementedError
+
+
+
+layer = tf.keras.layers.Dense(num_hidden_nodes[0], activation=activations_hidden_nodes[0],
+                              kernel_initializer=tf.keras.initializers.RandomNormal(),
+                              bias_initializer=tf.keras.initializers.RandomNormal())(X)
+for layerIndex in range(1, num_hidden_nodes):
+    layer = tf.keras.layers.Dense(num_hidden_nodes[0], activation=activations_hidden_nodes[0])(layer)
+savings = tf.keras.layers.Dense(A-1)
+
+
+
+
+
+def create_training_data():
+
 
 def firm(K, eta, alpha, delta):
     """Calculate return, wage and aggregate production.
@@ -78,19 +139,8 @@ def wealth(k, R, l, W):
     return fin_wealth, lab_wealth, tot_income
 
 
-
-
 def main():
-    seed = 1
-    num_hidden_nodes = [100, 100]
-    activations_hidden_nodes = [tf.nn.relu, tf.nn.relu]
-    optimizer = 'adam'
-    num_episodes = 50000
-    len_episodes = 10240
-    epochs_per_episode = 20
-    minibatch_size = 512
-    num_minibatch = int(len_episodes/minibatch_size)
-    lr = 1e-5
+
 
 
 
